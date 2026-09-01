d1e8509a64b3b94445425220429a091b93a5efe7
Ludwig Lierhammer <Ludwig.Lierhammer@dwd.de>
2026-09-01 14:19:28 +0200

    Refactor sys.path insertion and update configurations

--- docs/conf.py
+++ docs/conf.py
@@ -15,14 +15,15 @@ from __future__ import annotations
 import os
 import sys
 import warnings
+from pathlib import Path
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 # sys.path.insert(0, os.path.abspath('.'))
 
-sys.path.insert(0, os.path.abspath(".."))
-sys.path.insert(0, os.path.abspath("."))
+sys.path.insert(0, str((Path(__file__).parent.parent / "..").resolve()))
+sys.path.insert(0, str((Path(__file__).parent.parent / ".").resolve()))
 
 import glamod_marine_processing  # noqa
 
@@ -45,33 +46,47 @@ extensions = [
     "sphinx.ext.todo",
     "sphinx.ext.autosectionlabel",
     "sphinx.ext.intersphinx",
-    "nbsphinx",
+    "myst_nb",
     "IPython.sphinxext.ipython_console_highlighting",
     "sphinx_autodoc_typehints",
     "sphinx_copybutton",
-    "sphinx_book_theme",
 ]
 
-
-extlinks = {
-    "issue": ("https://github.com/glamod/glamod-marine-processing/issues/%s", "#%s"),
-    "pull": ("https://github.com/glamod/glamod-marine-processing/pull/%s", "#%s"),
-    "user": ("https://github.com/%s", "@%s"),
-}
+autosectionlabel_prefix_document = True
+autosectionlabel_maxdepth = 2
 
 autosummary_generate = True
 autodoc_typehints = "description"
 autodoc_typehints_format = "fully-qualified"
 autodoc_typehints_description_target = "documented_params"
+python_use_unqualified_type_names = True
 
-autosectionlabel_prefix_document = True
-autosectionlabel_maxdepth = 2
+# typehints_use_signature = True
+# typehints_fully_qualified = False
 
 napoleon_numpy_docstring = True
 napoleon_use_rtype = False
 napoleon_use_param = False
 napoleon_use_ivar = True
 napoleon_preprocess_types = True
+napoleon_type_aliases = {
+    "Iterable": ":term:`iterable`",
+    "Hashable": ":term:`hashable`",
+}
+
+# To ensure that underlined fields (e.g. `_field`) are shown in the docs.
+autodoc_default_options = {
+    "members": True,
+    "undoc-members": True,
+    "private-members": False,
+    "special-members": False,
+}
+
+extlinks = {
+    "issue": ("https://github.com/glamod/glamod-marine-processing/issues/%s", "#%s"),
+    "pull": ("https://github.com/glamod/glamod-marine-processing/pull/%s", "#%s"),
+    "user": ("https://github.com/%s", "@%s"),
+}
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
@@ -89,14 +104,14 @@ remove_from_toctrees = ["generated/*"]
 
 # General information about the project.
 project = "glamod_marine_processing"
-copyright = "2019-2025, Ludwig Lierhammer, Axel Andersson, Tina Leiding, Richard Cornes, Elizabeth Kent, Joseph Siddons and John Kennedy"
+copyright = "2019-2026, Ludwig Lierhammer, Axel Andersson, Tina Leiding, Richard Cornes, Elizabeth Kent, Joseph Siddons and John Kennedy"
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
 # built documents.
 #
 # The short X.Y version.
-version = glamod_marine_processing.__version__  # .split("+")[0]
+version = glamod_marine_processing.__version__
 # The full version, including alpha/beta/rc tags.
 release = glamod_marine_processing.__version__
 
@@ -114,118 +129,24 @@ language = "en"
 # directories to ignore when looking for source files.
 exclude_patterns = ["_build", "**.ipynb_checkpoints"]  # , "logo"]
 
-# The reST default role (used for this markup: `text`) to use for all
-# documents.
-# default_role = None
-
-# If true, '()' will be appended to :func: etc. cross-reference text.
-# add_function_parentheses = True
-
-# If true, the current module name will be prepended to all description
-# unit titles (such as .. function::).
-# add_module_names = True
-
-# If true, sectionauthor and moduleauthor directives will be shown in the
-# output. They are ignored by default.
-# show_authors = False
-
 # The name of the Pygments (syntax highlighting) style to use.
 pygments_style = "sphinx"
 
-# A list of ignored prefixes for module index sorting.
-# modindex_common_prefix = []
-
-# If true, keep warnings as "system message" paragraphs in the built documents.
-# keep_warnings = False
-
+# If true, `todo` and `todoList` produce output, else they produce nothing.
+todo_include_todos = False
 
 # -- Options for HTML output ----------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = "sphinx_book_theme"
+html_title = "glamod-marine-processing Official Documentation"
+html_short_title = "glamod_marine_processing"
+html_theme = "furo"
 
-# Theme options are theme-specific and customize the look and feel of a theme
-# further.  For a list of options available for each theme, see the
-# documentation.
-# html_theme_options = {}
-
-# does not work on rdt
-# html_theme_options = {'logo_only': True}
-
-
-# Add any paths that contain custom themes here, relative to this directory.
-# html_theme_path = []
-
-# The name for this set of Sphinx documents.  If None, it defaults to
-# "<project> v<release> documentation".
-# html_title = None
-
-# A shorter title for the navigation bar.  Default is the same as html_title.
-# html_short_title = None
-
-# The name of an image file (relative to this directory) to place at the top
-# of the sidebar.
-# html_logo = "logo/logo.png"
-
-# The name of an image file (within the static path) to use as favicon of the
-# docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
-# pixels large.
-# html_favicon = "logo/favicon.ico"
-
-# Add any paths that contain custom static files (such as style sheets) here,
-# relative to this directory. They are copied after the builtin static files,
-# so a file named "default.css" will overwrite the builtin "default.css".
-# html_static_path = ['static']
-
-# Add any extra paths that contain custom files (such as robots.txt or
-# .htaccess) here, relative to this directory. These files are copied
-# directly to the root of the documentation.
-# html_extra_path = []
-
-# If not '', a 'Last updated on:' timestamp is inserted at every page bottom,
-# using the given strftime format.
-# html_last_updated_fmt = '%b %d, %Y'
-
-# If true, SmartyPants will be used to convert quotes and dashes to
-# typographically correct entities.
-# html_use_smartypants = True
-
-# Custom sidebar templates, maps document names to template names.
-# html_sidebars = {}
-
-# Additional templates that should be rendered to pages, maps page names to
-# template names.
-# html_additional_pages = {}
-
-# If false, no module index is generated.
-# html_domain_indices = True
-
-# If false, no index is generated.
-# html_use_index = True
-
-# If true, the index is split into individual pages for each letter.
-# html_split_index = False
-
-# If true, links to the reST sources are added to the pages.
-# html_show_sourcelink = True
-
-# If true, "Created using Sphinx" is shown in the HTML footer. Default is True.
-# html_show_sphinx = True
-
-# If true, "(C) Copyright ..." is shown in the HTML footer. Default is True.
-# html_show_copyright = True
-
-# If true, an OpenSearch description file will be output, and all pages will
-# contain a <link> tag referring to it.  The value of this option must be the
-# base URL from which the finished HTML is served.
-# html_use_opensearch = ''
-
-# This is the file name suffix for HTML files (e.g. ".xhtml").
-# html_file_suffix = None
+# -- Options for HTMLHelp output ---------------------------------------
 
 # Output file base name for HTML help builder.
-htmlhelp_basename = "glamod-marine-processingdoc"
+htmlhelp_basename = "glamod_marine_processingdoc"
 
 # -- Options for manual page output ---------------------------------------
 
@@ -237,3 +158,11 @@ htmlhelp_basename = "glamod-marine-processingdoc"
 
 # disable warnings
 warnings.filterwarnings("ignore")
+
+# Example configuration for intersphinx: refer to the Python standard library.
+intersphinx_mapping = {
+    "numpy": ("https://numpy.org/doc/stable", None),
+    "pandas": ("https://pandas.pydata.org/pandas-docs/stable", None),
+    "python": ("https://docs.python.org/3/", None),
+    "xarray": ("https://docs.xarray.dev/en/stable", None),
+}
